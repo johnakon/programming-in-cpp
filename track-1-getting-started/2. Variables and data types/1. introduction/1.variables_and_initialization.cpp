de7a765e1139ee7248b1679b9b2688d69668f34c
johnakon <john.akonya@gmail.com>
2022-09-01 02:40:40 +0300

    Variables and Initialization

---
+++ track-1-getting-started/2. Variables and data types/1. introduction/1.variables_and_initialization.cpp
@@ -0,0 +1,17 @@
+// #include <iostream>
+
+// using std::cout;
+// using std::endl;
+
+// int main() {
+
+//     int balance; // declaring a variable
+
+//     cout << "My Account balance is : $" << balance << endl; // return s a garbage value
+
+//     balance = 1000;  // Initializing variable
+
+//     cout << "My Account balance is : $" << balance << endl;
+
+//     return 0;
+// }

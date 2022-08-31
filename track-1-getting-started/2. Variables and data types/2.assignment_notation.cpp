4803307a9682b9fc3e6f1aa61ca33a257cf3b34f
johnakon <john.akonya@gmail.com>
2022-09-01 02:06:15 +0300

    Assignment Notation

---
+++ track-1-getting-started/2. Variables and data types/2.assignment_notation.cpp
@@ -0,0 +1,17 @@
+// #include <iostream>
+
+// using std::cout;
+// using std::endl;
+
+// int main(){
+
+//     int balance = 1000;
+//     int withdrawal = 200;
+//     int current_balance = balance - withdrawal;
+
+//     cout << "Bank balance:= $" << balance << endl;
+//     cout << "Bank withdraw:= $" << withdrawal << endl;
+//     cout << "Curret bank balance:= $" << current_balance << endl;
+
+//     return 0;
+// }

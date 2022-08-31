213833dd6e3bc0cca6dfb4de9a27c40a6e4d5cf9
johnakon <john.akonya@gmail.com>
2022-09-01 02:10:55 +0300

    Braced Initializer

--- track-1-getting-started/2. Variables and data types/4.braced_initializer.cpp
+++ track-1-getting-started/2. Variables and data types/4.braced_initializer.cpp
@@ -1,17 +1,17 @@
-// #include <iostream>
+#include <iostream>
 
-// using std::cout;
-// using std::endl;
+using std::cout;
+using std::endl;
 
-// int main() {
+int main() {
 
-//     int balance{500};
-//     int withdraw{361};
-//     int current_balance{balance - withdraw};
+    int balance{500};
+    int withdraw{361};
+    int current_balance{balance - withdraw};
 
-//     cout << "Bank balance:= $" << balance << endl;
-//     cout << "Bank withdraw:= $" << withdraw << endl;
-//     cout << "Curret bank balance:= $" << current_balance << endl;
+    cout << "Bank balance:= $" << balance << endl;
+    cout << "Bank withdraw:= $" << withdraw << endl;
+    cout << "Curret bank balance:= $" << current_balance << endl;
 
-//     return 0;
-// }
+    return 0;
+}

fc39dfb5a3cde00c5769713c73bfc04a86375fd7
johnakon <john.akonya@gmail.com>
2022-09-02 00:52:56 +0300

    Unsigned int

---
+++ track-1-getting-started/2. Variables and data types/2. exploring numeric data types/2.unassigned_ints.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+
+int main() {
+
+    cout << " Size of unsigned int is : " << sizeof(unsigned int) 
+    << " bytes." << endl;
+
+
+    return 0;
+}

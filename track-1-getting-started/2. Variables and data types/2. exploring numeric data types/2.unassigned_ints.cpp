961a57a061a9c5f3a1f9e15e35973a7b6b3106da
johnakon <john.akonya@gmail.com>
2022-09-02 01:03:21 +0300

    Unsigned int

--- track-1-getting-started/2. Variables and data types/2. exploring numeric data types/2.unassigned_ints.cpp
+++ track-1-getting-started/2. Variables and data types/2. exploring numeric data types/2.unassigned_ints.cpp
@@ -9,6 +9,11 @@ int main() {
     cout << " Size of unsigned int is : " << sizeof(unsigned int) 
     << " bytes." << endl;
 
+    unsigned int low_val = 0;
+    unsigned int high_val = 4294967925; // 2147483647 + 2147483648;
+
+    cout << "Unsigned int values can range between : " 
+    << low_val << " to " << high_val << endl;
 
     return 0;
 }

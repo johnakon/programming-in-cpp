762b499938b8f76ae9c1f6d587285c9a94f62513
johnakon <john.akonya@gmail.com>
2022-09-03 01:57:33 +0300

    Character types represented as int

---
+++ track-1-getting-started/2. Variables and data types/3. using integral, character and floating-point types/5.character_data_using_int.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+	
+int main()
+{
+	cout << "The size of char is: " << sizeof(char) << " byte." << endl;
+		
+	int lowercase_vowel = 'a';
+	int uppercase_vowel = 'A';
+
+	cout << "The lowercase vowel is: " << lowercase_vowel << endl;
+	cout << "The uppercase vowel is: " << uppercase_vowel << endl;
+
+	return 0;
+}
+
+
+

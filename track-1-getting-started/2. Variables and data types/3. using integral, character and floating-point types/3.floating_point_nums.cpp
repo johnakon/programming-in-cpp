b5b8dbccd3accb1f43ff6034a97bce7172e4a0ff
johnakon <john.akonya@gmail.com>
2022-09-03 01:13:54 +0300

    Size of floating point values

---
+++ track-1-getting-started/2. Variables and data types/3. using integral, character and floating-point types/3.floating_point_nums.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(){
+
+    cout << "Size of float is : " << sizeof(float) << " bytes." << endl;
+    cout << "Size of double is : " << sizeof(double) << " bytes." << endl;
+    cout << "Size of long double is : " << sizeof(long double) << " bytes." << endl;
+
+    return 0;
+}

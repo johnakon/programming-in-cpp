288c9f2cfc38ea61635887c30cba3cda1e78325d
johnakon <john.akonya@gmail.com>
2022-09-03 01:19:10 +0300

    Working with some floating point constants

--- track-1-getting-started/2. Variables and data types/3. using integral, character and floating-point types/3.floating_point_nums.cpp
+++ track-1-getting-started/2. Variables and data types/3. using integral, character and floating-point types/3.floating_point_nums.cpp
@@ -9,5 +9,13 @@ int main(){
     cout << "Size of double is : " << sizeof(double) << " bytes." << endl;
     cout << "Size of long double is : " << sizeof(long double) << " bytes." << endl;
 
+    float balance = 1000.64F;
+    double earth_gravity = 9.807;
+    long double plancks_constant = 6.62607004e-34L;
+
+    cout << endl << "Balance : " << balance << endl;
+    cout << "Earth's gravity : " << earth_gravity << endl;
+    cout << "Planck's constant : " << plancks_constant << endl;
+
     return 0;
 }

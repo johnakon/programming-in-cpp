870e267e740402bf0ef0ccc0853df90635637cdd
johnakon <john.akonya@gmail.com>
2022-09-03 03:06:15 +0300

    Performing logical and arithmetic operations

---
+++ track-1-getting-started/2. Variables and data types/5. performing logical and arithmetic operations/operators_v02.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main()
+{
+  
+  float num_a = 78.87f, num_b = 8.456f;
+
+  cout << "num_a + num_a = " << num_a + num_b << endl;
+  cout << "num_a - num_a = " << num_a - num_b << endl;
+  cout << "num_a * num_a = " << num_a * num_b << endl;
+  cout << "num_a / num_a = " << num_a / num_b << endl;
+
+  return 0;
+}

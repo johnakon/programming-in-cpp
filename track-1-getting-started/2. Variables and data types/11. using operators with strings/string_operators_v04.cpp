2f469fc3456ed87b2f529b90665ea38d4fedd855
johnakon <john.akonya@gmail.com>
2022-09-03 04:00:36 +0300

    String operators

---
+++ track-1-getting-started/2. Variables and data types/11. using operators with strings/string_operators_v04.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using std::cout;
+using std::endl;
+using std::string;
+
+int main()
+{
+  string part_2 = "another opens";
+
+  string proverb = "When one door shuts " + part_2;
+
+  cout << "Concatenated string: " << proverb << endl;
+
+  return 0;
+}
